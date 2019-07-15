e7fea21943dde97e96351089131a4c376d2503d7
deEth007 <visimon430@gmail.com>
2019-07-16 02:44:17 +0900

    createNote, liquidateNote helper integrated

--- scripts/helper/mintNBurnNoteHelper.js
+++ scripts/helper/mintNBurnNoteHelper.js
@@ -1,4 +1,3 @@
-// const verifier = artifacts.require("verifier");
 const fs = require('fs');
 const BN = require('bn.js');
 const crypto = require('crypto');
@@ -15,14 +14,14 @@ const SCALING_FACTOR = new BN('1000000000000000000');
 function getCreateParams(owner, amount, type, viewKey, salt, isSmart){
   //all params should look like this "0001"(o), "0x0001"(x)
   let noteOwner = new BN(owner, 16).toString(16); //32 bytes = 256 bits
-  //TODO : if it is shorter then 32 bytes, index 1 returns ''
+  //TODO : if it is shorter then 16 bytes, index 1 returns ''
   let splittedNoteOwner = [noteOwner.slice(0, 32), noteOwner.slice(32)];
 
   let noteValue =  new BN(amount, 16).toString(16, 32); //16 bytes = 128 bits
   let noteType = new BN(type, 16).toString(16, 32); //16 bytes = 128 bits
 
   let noteViewKey = new BN(viewKey, 16).toString(16); //32 bytes = 256 bits
-  //TODO : if it is shorter then 32 bytes, index 1 returns ''
+  //TODO : if it is shorter then 16 bytes, index 1 returns ''
   let splittedNoteViewKey = [noteViewKey.slice(0, 32), noteViewKey.slice(32)];
 
   let noteSalt = new BN(salt, 16).toString(16, 32); //16 bytes = 128 bits
@@ -59,11 +58,13 @@ function test(){
   owner = "1aba488300a9d7297a315d127837be4219107c62c61966ecdf7a75431d75cc61";
   value = '6'
   type = '0'
-  viewKey = "1aba488300a9d7297a315d127837be4219107c62c61966ecdf7a75431d75cc61";
+  viewKey = "1aba488300a9d72s97a315d127837be4219107c62c61966ecdf7a75431d75cc61";
   salt = "c517f646255d5492089b881965cbd3da";
   isSmart = '0';
   let params = getCreateParams(owner, value, type, viewKey, salt, isSmart);
   printZokratesCommand(params);
+  // output
+  // ./zokrates compute-witness -a 28438254119279180143054153302986539534 216363634539208029509085300953461217659 6 0 35527165818681367460734522247605632578 33316299488818974410722173859617164385 35527165818681367460734522247605632578 33316299488818974410722173859617164385 261982333027672377144177477746906878938 0
 }
 
 // function test(){
